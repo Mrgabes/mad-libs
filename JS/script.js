2d8d0b11eb60938203c5aaf9b7a8ce1c5edfdb4b
Gabriel Rodriguez <mrgabes72@gmail.com>
2021-06-02 10:34:10 -0700

    Add .toUpperCase

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
+    $(".person1").text(person1Input.toUpperCase());
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
