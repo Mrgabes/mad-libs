487054a0719e88add7c63643f4759b4ffbb90db4
Gabriel Rodriguez <mrgabes72@gmail.com>
2021-06-02 09:54:31 -0700

    Add Jquery making output working correctly.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
+  $("#formOne").submit(function(event) {
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
@@ -7,13 +7,15 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $("person1").text(person1Input);
+    $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
 
-    $("#story").sho();
+    $("#story").show();
+
+    event.preventDefault();
   });
 });
